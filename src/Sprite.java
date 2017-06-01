450369c6d0847fe6bb6084352480333bef94914c
Andrew Briasco <andrew_briasco-stewart@student.wayland.k12.ma.us>
2017-06-01 09:24:28 -0400

    Basic SetuUp

---
+++ src/Sprite.java
@@ -0,0 +1,13 @@
+import org.newdawn.slick.Image;
+
+/**
+ * Created by andrew_briasco on 6/1/17.
+ */
+public class Sprite {
+    private int x, y;
+    private Image pic;
+    public Sprite(){
+        pic = new Image("res/blank.png")
+    }
+
+}
